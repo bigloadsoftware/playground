89429ce666670b4127f1225bbe4b4832448b4162
OverDriveZ <64517916+OverDriveZ@users.noreply.github.com>
2024-02-14 23:00:18 +0100

    nightstalker idle animations - iter 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 200
 var screen_size 
+var last_direction_faced = "idle_down"
 
 
 func _ready():
@@ -14,23 +15,39 @@ func _process(delta):
 		velocity.x = 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1
-	if Input.is_action_pressed("move_down"):
-		velocity.y = 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y = 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		if velocity.x > 0:
+		if velocity.x > 0 && velocity.y > 0:
 			$AnimatedSprite2D.play("run_right")
-		if velocity.x < 0:
+			last_direction_faced = "idle_right"
+		if velocity.x > 0 && velocity.y < 0:
+			$AnimatedSprite2D.play("run_right")
+			last_direction_faced = "idle_right"
+		if velocity.x < 0 && velocity.y > 0:
 			$AnimatedSprite2D.play("run_left")
-		if velocity.y > 0:
+			last_direction_faced = "idle_left"
+		if velocity.x < 0 && velocity.y < 0:
+			$AnimatedSprite2D.play("run_left")
+			last_direction_faced = "idle_left"
+		if velocity.x == 0 && velocity.y > 0:
 			$AnimatedSprite2D.play("run_down")
-		if velocity.y < 0:
+			last_direction_faced = "idle_down"
+		if velocity.x == 0 && velocity.y < 0:
 			$AnimatedSprite2D.play("run_up")
+			last_direction_faced = "idle_up"
+		if velocity.x > 0 && velocity.y == 0:
+			$AnimatedSprite2D.play("run_right")
+			last_direction_faced = "idle_right"
+		if velocity.x < 0 && velocity.y == 0:
+			$AnimatedSprite2D.play("run_left")
+			last_direction_faced = "idle_left"
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play(last_direction_faced)
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
